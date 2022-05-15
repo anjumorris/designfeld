000c079dae26df24ff8b5cf6d18c3ce1e94ea71b
Vincent Marklynn <vincent.marklynn@gmail.com>
2022-05-15 10:06:40 -0700

    Added basic VideoJS player in video.html; Added basic map to map scene to timestamp in video_scripts.js

---
+++ video_scripts.js
@@ -0,0 +1,10 @@
+const scenesNumber = 22;
+const sceneTimestamp = [0, 35, 122, 256, 283, 369, 404, 530, 579, 631, 669,
+  816, 885, 991, 1038, 1070, 1105, 1160, 1214, 1267, 1317, 1325];
+
+const sceneToTimestamp = new Map();
+
+for(let i = 0; i < sceneTimestamp.length; i++)
+{
+  sceneToTimestamp.set(i+1, sceneTimestamp[i]);
+}
