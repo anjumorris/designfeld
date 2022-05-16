17e677158ae38b4c35e68e436461e6f285dd7deb
Ulysses Lin <utemoc@gmail.com>
2022-05-15 22:11:23 -0700

    UL: Adds ep times and scene thumbs

--- circle.js
+++ circle.js
@@ -20,6 +20,195 @@ function mainChars(toCheck) {
     return ["JERRY", "ELAINE", "GEORGE", "KRAMER"].includes(toCheck);
 }
 
+var mouseover = function (i, scene) {
+    if (typeof scene === 'undefined') {
+      return;
+    }
+
+    d3.select(this)
+        .transition()
+        .duration(300)
+        .attr('opacity', 0.2)
+
+    svg.select('#scene_intro')
+        .transition()
+        .duration(100)
+        .attr('opacity', 0)
+
+    svg.select('#scene_metadata')
+        .transition()
+        .duration(100)
+        .attr('opacity', 1)
+
+    svg.select('#scene_metadata_title')
+        .text('Scene ' + scene.scene)
+        .transition()
+        .duration(100)
+        .attr('opacity', 1)
+
+    svg.select('#scene_metadata_name')
+        .text(scene.scene_desc)
+        .transition()
+        .duration(100)
+        .attr('opacity', 1)
+
+    svg.select('#scene_metadata_times')
+        .text('[' + scene.start + ' - ' + scene.end + ']')
+        .transition()
+        .duration(100)
+        .attr('opacity', 1)
+
+    svg.select('#scene_thumb')
+        .attr('xlink:href', '/scene_thumbs/scene_' + scene.scene + '.png')
+}
+
+var mouseleave = function (i, scene) {
+    if (typeof scene === 'undefined') {
+      return;
+    }
+
+    d3.select(this)
+        .transition()
+        .duration(100)
+        .attr('opacity', 0)
+}
+
+svg.attr("width", 1000).attr("height", 1100);
+
+d3.csv("/scenes.csv").then(function(sceneData) {
+    svg.selectAll()
+        .data(sceneData)
+        .enter()
+        .append("path")
+        .attr("transform", "translate(500,600)")
+        .attr('opacity', 0)
+        .attr("d", d3.arc()
+            .innerRadius( 250 )
+            .outerRadius( 500 )
+            .startAngle(function(d) { return t(parse(d.start)) * cEnd; })
+            .endAngle(function(d) { return (t(parse(d.end)) + 0.0005) * cEnd; })
+        )
+        .attr('id', function(d) {
+            return 'scene_' + d.scene; // id="scene_2"
+        })
+        .attr('class', 'scene')
+        .attr('fill', '#808080')
+        .on('mouseover', mouseover)
+        .on('mouseleave', mouseleave)
+
+    svg.selectAll()
+        .data(sceneData)
+        .enter()
+        .append("path")
+        .attr("transform", "translate(500,600)")
+        .attr('opacity', 0.5)
+        .attr("d", d3.arc()
+            .innerRadius(function(d) { return d.start == '0:00' ? 300 : 350; })
+            .outerRadius(function(d) { return d.start == '0:00' ? 550 : 500; })
+            .startAngle(function(d) { return t(parse(d.start)) * cEnd; })
+            .endAngle(function(d) { return (t(parse(d.start)) + 0.0005) * cEnd; })
+        )
+        .attr('class', 'scene_divider')
+        .attr('fill', '#808080')
+
+    svg.append("g")
+        .attr('id', 'scene_intro')
+        // .call(g => g.append("rect")
+        // .attr('class', 'lbox')
+        // .attr("x", 100)
+        // .attr("y", 40)
+        // .attr('rx', 3)
+        // .attr('width', '20')
+        // .attr('height', '20')
+        // .style("fill", "#87a1ff"))
+        .call(g => g.append("text")
+        .text('Select a scene')
+        .style("font-family", "lato")
+        .attr('id', 'scene_intro_content')
+        .attr('font-size', '40')
+        .attr("x", 390)
+        .attr("y", 510)
+        .style("fill", "black"))
+        .call(g => g.append("text")
+        .style("font-family", "lato")
+        .text('Hover over a section of the wheel')
+        .attr('id', 'scene_intro_content')
+        .attr("x", 390)
+        .attr("y", 550)
+        .style("fill", "black"))
+        .call(g => g.append("text")
+        .style("font-family", "lato")
+        .text('and click to see more information')
+        .attr('id', 'scene_intro_content')
+        .attr("x", 390)
+        .attr("y", 570)
+        .style("fill", "black"))
+
+    svg.append("g")
+        .attr('id', 'scene_metadata')
+        .attr('opacity', 0)
+        // .call(g => g.append("rect")
+        // .attr('class', 'lbox')
+        // .attr("x", 100)
+        // .attr("y", 50)
+        // .attr('rx', 3)
+        // .attr('width', '20')
+        // .attr('height', '20')
+        // .style("fill", "#87a1ff"))
+        .call(g => g.append("text")
+        .text('Scene ')
+        .style("font-family", "lato")
+        .attr('id', 'scene_metadata_title')
+        .attr('font-size', '40')
+        .attr("x", 390)
+        .attr("y", 510)
+        .style("fill", "black"))
+        .call(g => g.append("text")
+        .style("font-family", "lato")
+        .text('')
+        .attr('id', 'scene_metadata_name')
+        .attr('font-size', '24')
+        .attr("x", 390)
+        .attr("y", 550)
+        .style("fill", "black"))
+        .call(g => g.append("text")
+        .style("font-family", "lato")
+        .text('')
+        .attr('id', 'scene_metadata_times')
+        .attr("x", 390)
+        .attr("y", 570)
+        .style("fill", "black"))
+
+    svg.append("g")
+        .attr('id', 'ep_times')
+        .attr('opacity', 1)
+        .call(g => g.append("text")
+        .text('Start: 0:00-->')
+        .style("font-family", "lato")
+        .attr('id', 'ep_start_time')
+        .attr('font-size', '12')
+        .attr("x", 510)
+        .attr("y", 75)
+        .style("fill", "black"))
+        .call(g => g.append("text")
+        .style("font-family", "lato")
+        .text('-->End: 22:49')
+        .attr('id', 'ep_end_time')
+        .attr('font-size', '12')
+        .attr("x", 420)
+        .attr("y", 100)
+        .style("fill", "black"))
+
+    svg.append("g")
+        .attr('opacity', 1)
+        .call(g => g.append('image')
+        .attr('id', 'scene_thumb')
+        .attr('x', 350)
+        .attr('y', 600)
+        .attr('width', 320)
+        .attr('height', 180)
+        .attr('xlink:href', '/scene_thumbs/scene_1.png'))
+});
 
 // Use the speakers.csv for speakers-only data
 d3.csv("/speakers.csv", function (d) {
@@ -47,7 +236,6 @@ d3.csv("/speakers.csv", function (d) {
     //     .domain(d3.extent([parse(epStartTime), parse(epEndTime)]))
     //     .range([0, 1]);
 
-    svg.attr("width", 1000).attr("height", 1000);
 
     var arcs = {};
     // data.forEach(function(d) {
@@ -65,7 +253,7 @@ d3.csv("/speakers.csv", function (d) {
         .data(data)
         .enter()
         .append("path")
-        .attr("transform", "translate(500,500)")
+        .attr("transform", "translate(500,600)")
         .attr('opacity', 1)
         .attr("d", d3.arc()
             .innerRadius( 400 )
